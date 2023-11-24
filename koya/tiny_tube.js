217d67064b1ad8412dd37cb53122e9220fa45c11
syndiate <67703687+syndiate@users.noreply.github.com>
2023-11-24 14:09:23 -0500

    Optimize v3

--- koya/tiny_tube.js
+++
@@ -1,2 +0,0 @@
-var newURL = chrome.extension.getURL("playground/index.html");
-chrome.tabs.create({ url: newURL });
--- v3core_popupmenu_210515.js
+++
@@ -1,4 +0,0 @@
-var discordButton = document.querySelector('#discord');
-discordButton.onclick = function() {
-  window.open("https://discord.gg/Bsvy7GbYZt");
-};
